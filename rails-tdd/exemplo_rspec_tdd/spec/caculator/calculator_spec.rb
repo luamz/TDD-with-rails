ce5713a48debdd10a0df1ba227fec3749fd75e89
luamz <luamsilva@id.uff.br>
2021-04-15 10:56:55 -0300

    Basic Specs

--- rails-tdd/exemplo_rspec_tdd/spec/caculator/calculator_spec.rb
+++ rails-tdd/exemplo_rspec_tdd/spec/caculator/calculator_spec.rb
@@ -1,15 +1,19 @@
 require 'calculator'
 
-describe Calculator do
-    context 'sum for 2 numbers' do
+describe Calculator, 'with simple operations' do
+    context '#sum' do
         it 'with positive numbers' do
-            calc = Calculator.new
-            result = calc.sum(3,7)
+            result = subject.sum(3,7)
             expect(result).to eq(10)
         end
-        specify 'with negative numbers' do
-            calc = Calculator.new
-            result = calc.sum(-5,7)
+
+        it 'with negative numbers' do
+            result = subject.sum(-5,-7)
+            expect(result).to eq(-12)
+        end
+
+        it 'with integers' do
+            result = subject.sum(-5,7)
             expect(result).to eq(2)
         end
     end
