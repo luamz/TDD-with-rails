e2a08fda2ce0fa85e70fa0b7e47bfaf1e7372029
luamz <luamsilva@id.uff.br>
2021-04-20 14:56:24 -0300

    Mocks

--- rails-tdd/exemplo_rspec_tdd/spec/testing_doubles/user_spec.rb
+++ rails-tdd/exemplo_rspec_tdd/spec/testing_doubles/user_spec.rb
@@ -5,4 +5,12 @@ describe 'Testing Double' do
     puts user.name
     puts user.password
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive_messages(name:"Liam",password: 'secret')
+    puts user.name
+    puts user.password
+    user.abc
+  end
 end
